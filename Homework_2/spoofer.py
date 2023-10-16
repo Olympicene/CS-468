d7d23dfdf3b99e8d4757311be7c604840812a923
Olympicene <kevinjcord@gmail.com>
2023-10-16 01:38:31 -0500

    update

--- Homework_2/spoofer.py
+++ Homework_2/spoofer.py
@@ -5,13 +5,14 @@ def send_packet(src_ip, dst_ip, dst_port, payload):
 	l2 = Ether()
 	l3 = IP(dst=dst_ip, src=src_ip)
 	l4 = UDP(dport=dst_port)
-	data = "This is a thing that is a thing"
-	payload = Raw(load=data)
 
-	packet = l2/l3/l4/payload
+
+	if (len(payload) > 150):
+		exit(0)
+
+	packet = l2/l3/l4/Raw(load=payload)
 	sendp(packet)
-
 	return 0
 
-send_packet("185.199.108.153", "162.159.133.234", 6969, 2343)
+send_packet("185.199.108.153", "162.159.133.234", 6969, "This is a thing that is a thing that is at hing that is thign that is athing")
 
